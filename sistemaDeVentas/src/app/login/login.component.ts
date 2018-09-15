14587546ad210961c0fdff44d067b4ef758deb73
jdmartinezrios <jdmartinez859@misena.edu.co>
2018-09-15 10:26:27 -0500

    se corrige el login

--- sistemaDeVentas/src/app/login/login.component.ts
+++ sistemaDeVentas/src/app/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var $:any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 
-declare var $:any;
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router) { }
